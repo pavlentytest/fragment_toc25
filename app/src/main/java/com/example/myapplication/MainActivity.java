d9b732e91a9ae5ac2ae1dbfc965e65262dab8bb9
Pavel Stepanov <impetus@yandex.ru>
2024-12-04 18:44:44 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn1;
+    private Button btn2;
+    private FragmentManager fm;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn1 = findViewById(R.id.button);
+        btn2 = findViewById(R.id.button2);
+        fm = getSupportFragmentManager();
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeFragment(new FirstFragment());
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeFragment(new SecondFragment());
+            }
+        });
+    }
+    public void changeFragment(Fragment f) {
+        // транзакция
+        FragmentTransaction transaction = fm.beginTransaction();
+        transaction.replace(R.id.fragmentContainerView,f);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+
+}
